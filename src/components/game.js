36d4a1554ecf33ba90362a5ede2e1cba38119008
Mary Conley <Mary@Marys-MacBook-Pro.local>
2018-11-08 09:31:09 -0800

    second commit

--- src/components/game.js
+++ src/components/game.js
@@ -2,17 +2,29 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
-}
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guesses: [10,15,25],
+            answer: 30
+        };
+    }
 
+    guessInputHandler()
+
+    render() {
+        
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback="Make your guess!" />
+                <GuessCount count={3} />
+                <GuessList guesses={[10, 15, 25]} />
+            </div>
+        );
+    }
+}
