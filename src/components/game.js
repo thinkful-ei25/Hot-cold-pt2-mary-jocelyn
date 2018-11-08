e38500bdf76af1d0667de3130b6bef484ea179a7
Mary Conley <Mary@Marys-MacBook-Pro.local>
2018-11-08 09:59:54 -0800

    third commit

--- src/components/game.js
+++ src/components/game.js
@@ -9,21 +9,36 @@ export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            guesses: [10,15,25],
-            answer: 30
+            guesses: [10, 15, 25],
+            answer: 30,
+            feedback: 'Make your guess!'
         };
+    }
+
+    userMakesGuess(guess) {
+        if (typeof guess !== 'number') {
+            alert('Please input a NUMBER');
+        }
+        if(userGuess < 0 || userGuess > 101){
+            alert('please choose a number between zero and 100');
+            return true;
+            
+        else if(guess === answer)
+
+        }
+
     }
 
     guessInputHandler()
 
     render() {
-        
+
         return (
             <div>
                 <Header />
-                <GuessSection feedback="Make your guess!" />
-                <GuessCount count={3} />
-                <GuessList guesses={[10, 15, 25]} />
+                <GuessSection feedback={this.state.feedback} />
+                <GuessCount count={this.state.guesses.length} />
+                <GuessList guesses={this.state.guesses} />
             </div>
         );
     }
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -5,8 +5,8 @@ import './guess-form.css';
 export default function GuessForm(props) {
     return (
         <form>
-            <input type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
+            <input type="number" name="userGuess" id="userGuess"
+                className="text" maxLength="3" autoComplete="off" min="1" max="100"
                 placeholder="Enter your Guess" required />
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
